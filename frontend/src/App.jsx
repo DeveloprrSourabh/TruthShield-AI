8d7bbce81780b3e7a83315f9bee3d04bb3b9a9f4
Sourabh <114208070+DeveloprrSourabh@users.noreply.github.com>
2026-09-21 21:39:11 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     setOpenSources({});
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/verify", {
+      const response = await fetch("https://truthshield-ai-1-gsvr.onrender.com/verify", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
